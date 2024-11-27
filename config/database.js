ddbbe233a9747adcfe5d2fa795442e6dd7c03aa7
varun-101 <varunchandwani4@outlook.com>
2024-11-27 13:23:38 +0530

    Updated database.js to use connection string

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Used CockroachDB to host the Database
 const pool = new Pool({
-  connectionString: process.env.connectionString || 'postgresql://varun:gfVjUSct2X-HvE1W0YvLig@yeti-molerat-8089.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/school_management?sslmode=verify-full'
+  connectionString: process.env.connectionString || ''
 })
 
 export default pool; 
